794dcb97aa651282c11da8cd527f10d31d58d470
eduardotwk <54043365+eduardotwk@users.noreply.github.com>
2020-05-18 14:45:02 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
-<h1>{{ __('Contact') }}</h1>
+<h1>HOLA LARAGON</h1>
 
-	<form method="POST" action="{{route('contact')}}">
-		@csrf
-		<input name="name" placeholder="Nombre.." value="{{old('name')}}" ><br>
-		{!! $errors->first('name', '<small>:message</small><br>'	) !!}
-
-		<input type="text" name="Email" placeholder="Correo.." value="{{ old('email') }}"><br>
-		{!! $errors->first('Email', '<small>:message</small><br>'	) !!}
-
-		<input name="subject" placeholder="Asunto.." value="Asunto de prueba.." value="{{ old('subject') }}"><br>
-		{!! $errors->first('subject', '<small>:message</small><br>'	) !!}
-
-		<textarea name="content" placeholder="Mensaje..">{{old('content')}}</textarea><br>
-		{!! $errors->first('content', '<small>:message</small><br>'	) !!}
-
-		<button>Enviar</button>
-	</form>
